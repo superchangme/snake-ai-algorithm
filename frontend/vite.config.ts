e8d31b2fb56fdceb441dbe39063e3d4843ea7417
superchangme <superchangme@126.com>
2026-03-11 23:50:25 +0800

    feat: 添加 vite 代理配置

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   server: {
-    port: 3000
+    port: 3000,
+    proxy: {
+      '/init': 'http://localhost:8087',
+      '/move': 'http://localhost:8087',
+      '/gameover': 'http://localhost:8087'
+    }
   }
 })
